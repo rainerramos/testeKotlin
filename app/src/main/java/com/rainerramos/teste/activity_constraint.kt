c85c81f2e5fa695edc56186e83efc1d2df3e643a
Rainer Ramos <rainermr89@hotmail.com>
2021-09-03 15:44:00 -0300

    ConstraintLayout

---
+++ app/src/main/java/com/rainerramos/teste/activity_constraint.kt
@@ -0,0 +1,11 @@
+package com.rainerramos.teste
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_constraint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint)
+    }
+}
